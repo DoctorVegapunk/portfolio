96b6f6c70dd13913d69e4a91b468a5901e17490d
leviNzamba <levinzamba@gmail.com>
2022-12-15 16:23:24 +0300

    initial

---
+++ Desktop/Upwork Projects/Radio Station/Radio Station/web/svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+        adapter: adapter({
+            pages: 'public',
+            assets: 'public',
+            fallback: "404.html",
+            precompress: false
+		}),
+    },
+	preprocess: [
+		preprocess({
+		  postcss: true,
+		}),
+	],
+};
+
+export default config;
